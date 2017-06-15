f07ab69b8836a8bd80ce2a8abb93d273685b0561
Selmir Hajruli <selmir.hajruli@gerber-systems.ch>
2017-06-15 09:19:33 +0200

    Fixed MessagesController namespace

--- src/TTL.Bot/Controllers/MessagesController.cs
+++ src/TTL.Bot/Controllers/MessagesController.cs
@@ -1,12 +1,12 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web.Http;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
-
-namespace TTL.Bot
+﻿namespace TTL.Bot.Controllers
 {
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
