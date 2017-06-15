1247ee4a7e1727d6c13e5dd07cb5a729950141a4
Selmir Hajruli <selmir.hajruli@gerber-systems.ch>
2017-06-15 12:59:30 +0200

    Implemented responses entities

---
+++ src/TTL.Bot/Reponses/LocationsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TTL.Bot.Reponses
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class LocationsResponse
+    {
+        public List<Station> Stations { get; set; }
+    }
+}
---
+++ src/TTL.Bot/Reponses/StationboardReponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TTL.Bot.Reponses
+{
+    using System;
+    using System.Collections.Generic;
+    using Models;
+    using Newtonsoft.Json;
+
+    [Serializable]
+    public class StationboardReponse
+    {
+        [JsonProperty(PropertyName = "Stationboard")]
+        public List<Stationboard> Stationboards { get; set; }
+
+    }
+}
